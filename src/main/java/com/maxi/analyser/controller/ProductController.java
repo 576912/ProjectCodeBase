315b2f9eab386908225ecad1eb45c87587cdd75b
621789 <PrathibhaHV@maximus.com>
2026-01-23 14:28:55 +0530

    updated report generating path

--- src/main/java/com/maxi/analyser/controller/ProductController.java
+++ src/main/java/com/maxi/analyser/controller/ProductController.java
@@ -25,7 +25,6 @@ public class ProductController {
     @GetMapping
     public List<Product> getProducts() {
         log.info("Get all products method is called..");
-        log.info("Added log");
         //adding sample comment
         return productService.getAllProducts();
 
