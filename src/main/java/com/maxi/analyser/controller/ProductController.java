fa6cc08b745f5b053b20bdb770d9c2add0a3e690
621789 <PrathibhaHV@maximus.com>
2026-01-28 11:15:40 +0530

    Modified product controller

--- src/main/java/com/maxi/analyser/controller/ProductController.java
+++ src/main/java/com/maxi/analyser/controller/ProductController.java
@@ -26,6 +26,7 @@ public class ProductController {
     public List<Product> getProducts() {
         log.info("Get all products method is called..");
         //adding sample comment
+
         return productService.getAllProducts();
 
     }
